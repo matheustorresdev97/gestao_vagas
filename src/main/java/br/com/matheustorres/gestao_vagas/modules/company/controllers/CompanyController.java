bbf8f14143784e3e953bca9b1d35e11623a3d0d1
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-12 18:50:34 -0300

    criando entidades, useCases e controllers

--- src/main/java/br/com/matheustorres/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/br/com/matheustorres/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -1,4 +1,4 @@
-package br.com.matheustorres.gestao_vagas.modules.candidate;
+package br.com.matheustorres.gestao_vagas.modules.company.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,22 +7,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.matheustorres.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.matheustorres.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.matheustorres.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/candidate")
-public class CandidateController {
+@RequestMapping("/company")
+public class CompanyController {
+
   @Autowired
-  private CreateCandidateUseCase createCandidateUseCase;
+  private CreateCompanyUseCase createCompanyUseCase;
 
   @PostMapping("/")
-  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+  public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity) {
+
     try {
-      var result = createCandidateUseCase.execute(candidateEntity);
+      var result = this.createCompanyUseCase.execute(companyEntity);
       return ResponseEntity.ok().body(result);
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
+
 }
