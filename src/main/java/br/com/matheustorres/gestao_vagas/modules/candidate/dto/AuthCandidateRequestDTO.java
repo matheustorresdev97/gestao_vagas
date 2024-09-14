b58271d46aed7b12914f7ced005f5f4f8c48c449
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-14 20:49:16 -0300

    autenticação e autorização

---
+++ src/main/java/br/com/matheustorres/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,4 @@
+package br.com.matheustorres.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+}
--- src/main/java/br/com/matheustorres/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/matheustorres/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.matheustorres.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.matheustorres.gestao_vagas.exceptions.UserFoundException;
@@ -9,14 +10,20 @@ import br.com.matheustorres.gestao_vagas.modules.candidate.CandidateRepository;
 
 @Service
 public class CreateCandidateUseCase {
-    @Autowired
+  @Autowired
   private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
         candidateEntity.getEmail()).ifPresent(user -> {
           throw new UserFoundException();
         });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
 
     return this.candidateRepository.save(candidateEntity);
   }
