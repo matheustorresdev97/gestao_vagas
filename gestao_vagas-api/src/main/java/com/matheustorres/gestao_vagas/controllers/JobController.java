99b69ddd5c91882e72f801bd94717a9e367608b4
matheustorresdev97 <matheustorresdev@gmail.com>
2024-11-21 18:57:12 -0300

    :recycle: refatorando JobController

--- gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/controllers/JobController.java
+++ gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/controllers/JobController.java
@@ -1,5 +1,7 @@
 package com.matheustorres.gestao_vagas.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import com.matheustorres.gestao_vagas.dtos.JobRequestDTO;
 import com.matheustorres.gestao_vagas.models.JobModel;
 import com.matheustorres.gestao_vagas.services.JobService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -21,9 +24,12 @@ public class JobController {
     JobService jobService;
 
     @PostMapping
-    public void createJob(@Valid @RequestBody JobRequestDTO jobRequestDTO) {
+    public void createJob(@Valid @RequestBody JobRequestDTO jobRequestDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
+
         var jobModel = new JobModel();
         BeanUtils.copyProperties(jobRequestDTO, jobModel);
+        jobModel.setCompanyId(UUID.fromString(companyId.toString()));
         jobService.save(jobModel);
     }
 }
