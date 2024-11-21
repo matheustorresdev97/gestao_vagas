38ff303cad29821a927533412dbdb89bfc5f199f
matheustorresdev97 <matheustorresdev@gmail.com>
2024-11-21 12:18:30 -0300

    :tada: estrutura inicial

---
+++ gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/controllers/CandidateController.java
@@ -0,0 +1,21 @@
+package com.matheustorres.gestao_vagas.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheustorres.gestao_vagas.models.CandidateModel;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping
+    public void create(@Valid @RequestBody CandidateModel candidateModel) {
+        System.out.println("Candidato");
+        System.out.println(candidateModel.getEmail());
+    }
+}
