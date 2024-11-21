6cf760658d3266453d47f056c4fb6bc66c29ce79
matheustorresdev97 <matheustorresdev@gmail.com>
2024-11-21 18:52:36 -0300

    :zap: recuperando jwt e validando token

--- gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/controllers/AuthCompanyController.java
+++ gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/controllers/AuthCompanyController.java
@@ -1,5 +1,8 @@
 package com.matheustorres.gestao_vagas.controllers;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,6 +50,7 @@ public class AuthCompanyController {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
             var token = JWT.create().withIssuer("javagas")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                     .withSubject(company.getId().toString()).sign(algorithm);
 
             return ResponseEntity.ok().body(token);
