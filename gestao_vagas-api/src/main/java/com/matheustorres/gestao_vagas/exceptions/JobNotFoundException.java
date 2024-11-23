e3db1d2c41c6009f831e19671c50dc874c977b8a
matheustorresdev97 <matheustorresdev@gmail.com>
2024-11-23 12:58:16 -0300

    Criando teste candidato não encontrado

---
+++ gestao_vagas-api/src/main/java/com/matheustorres/gestao_vagas/exceptions/JobNotFoundException.java
@@ -0,0 +1,7 @@
+package com.matheustorres.gestao_vagas.exceptions;
+
+public class JobNotFoundException extends RuntimeException{
+    public JobNotFoundException(){
+        super("Job not found");
+    }
+}
